271a94e421ba3ba5cb6772e944cb6665d051b7ef
Petr Janoš <koksa.janos@gmail.com>
2020-11-22 01:56:28 +0100

    final 1.1.

--- du1.py
+++ du1.py
@@ -5,27 +5,26 @@ from turtle import screensize, speed, penup, pendown, setpos, seth, forward, exi
 M = 10000000
 
 # TODO: Funkce pro jednotlivá zobrazení
-
-
 def Marinovo(M, R, meritko):
-    """ Vytvoří seznam souřadnic k Marinovu zobrazení """
+    """ Vykreslí Marinovo zobrazení """
     list_poledniky = []
     list_rovnobezky = []
 
+    # * Vzorce přepočteny na px a zaokrouhleny na 1 des. místo
     # poledniky
     for deg_x in range(-180, 190, 10):
-        poledniky_x = round(R * (radians(deg_x)) * M / meritko / 3, 1)
+        poledniky_x = round(R * (radians(deg_x)) * M / meritko / 3, 1) 
         list_poledniky.append(poledniky_x)
     # rovnobezky
     for deg_y in range(-90, 100, 10):
         rovnobezky_y = round(R * (radians(deg_y)) * M / meritko / 3, 1)
         list_rovnobezky.append(rovnobezky_y)
 
-    # * KRESLENÍ
+    # * vykreslení zobrazení
     delka_polednik = abs(max(list_rovnobezky) - min(list_rovnobezky))
     delka_rovnobezka = abs(max(list_poledniky) - min(list_poledniky))
     speed(10)
-    screensize(1260, 891)
+    screensize(1260, 891) # nastavení plátna
     for i in range(37):
         penup()
         setpos(list_poledniky[i], list_rovnobezky[0])
@@ -40,9 +39,8 @@ def Marinovo(M, R, meritko):
         forward(delka_rovnobezka)
         penup()
 
-
 def Marinovo_body(body_X, body_Y):
-    M = 10000000  # km » mm: 1.000.000 + převod měřítka: *10
+    """ Vykreslí zadané body do zobrazení. Vstupem seznamy souřadnic bodů."""
 
     for p in range(len(body_X)):
         penup()
@@ -51,12 +49,12 @@ def Marinovo_body(body_X, body_Y):
         dot(10)
     exitonclick()
 
-
 def Postelovo(M, R, meritko):
     """ Vykreslí Postelovo zobrazení """
     list_poledniky = []
     list_rovnobezky = []
 
+    # * Vzorce přepočteny na px a zaokrouhleny na 1 des. místo
     for deg_x in range(-180, 190, 10):
         poledniky_x = round(R * (radians(deg_x)) * M / meritko / 3, 1)
         list_poledniky.append(poledniky_x)
@@ -65,11 +63,10 @@ def Postelovo(M, R, meritko):
         rovnobezky_y = round(R * (radians(deg_y)) * M / meritko / 3, 1)
         list_rovnobezky.append(rovnobezky_y)
 
-    # * KRESLENÍ
+    # * vykreslí zobrazení 
     delka_polednik = abs(max(list_rovnobezky) - min(list_rovnobezky))
-
     speed(10)
-    screensize(1260, 891)
+    screensize(1260, 891) # velikost plátna
     for deg in range(0, 370, 10):
         seth(deg)
         forward(delka_polednik)
@@ -83,8 +80,8 @@ def Postelovo(M, R, meritko):
         pendown()
         circle(list_poledniky[x])
 
-
 def Postelovo_body(body_X, body_Y):
+    """ Vykreslí zadané body do zobrazení. Vstupem seznamy souřadnic bodů. """
 
     for p in range(len(body_X)):
         penup()
@@ -93,74 +90,74 @@ def Postelovo_body(body_X, body_Y):
         dot(10)
     exitonclick()
 
-
 # TODO: Uživatelské vstupy
-# Zobrazení
+# * Výběr zobrazení 
 while True:
     zobrazeni_input = str(input('Nabízená zobrazení: \n'
-                                ' Ma = Marinovo zobrazení \n'  # ? válcová tečná
-                                ' Po = Postelovo zobrazení \n'  # ? azimutální
-                                ' La = Lambertovo zobrazení \n'  # ? kuželová
-                                ' Sa = Sansonovo zobrazení \n'  # ? nepravá
+                                ' Ma = Marinovo zobrazení (válcové)\n'  
+                                ' Po = Postelovo zobrazení (azimutální)\n'  
+                                ' La = Lambertovo zobrazení \n' 
+                                ' Sa = Sansonovo zobrazení \n'  
                                 '\nZadejte zkratku: '))
     if zobrazeni_input == "Ma" or zobrazeni_input == "Po" or zobrazeni_input == "La" or zobrazeni_input == "Sa":
         break
     else:
-        print("Nesprávný vstup, zadejte zobrazení z nabídky:")
+        print("!!! Nesprávný vstup, zadejte zobrazení z nabídky !!! \n")
 
-# Poloměr
+# * Zadání poloměru 
 while True:
-    R = float(
-        input("\nZadejte poloměr Země (zadejte nulu pro výchozí poloměr 6371.11): \n"))
-    if R < 0:
+    R = float(input("\nZadejte poloměr Země (zadejte nulu pro výchozí poloměr 6371.11): \n"))
+    if R < 0: # chybně zadaný poloměr
         print("zadaný poměr je chybný, zkuste to znovu \n")
         continue
-    elif R == 0:
+    elif R == 0: # použití výchozího poloměru
         R = 6371.11
         break
     else:
         break
 
-# Měřítko
+# * Zadání měřítka
 while True:
     meritko = int(input("\nZadejte měřítko (pro výchozí 1: 120 000 000 zadejte nulu): \n"
                         "1 : "))
-    if meritko < 0:
+    if meritko < 0: # chybně zadané měřítko
         print("zadané měřítko je chybné, zkuste to znovu \n")
         continue
-    elif meritko == 0:
+    elif meritko == 0: # použití výchozího měřítka
         meritko = 120000000
         break
     else:
         break
 
-# ! body
+# * Zadání bodů
 body_X = []
 body_Y = []
 
-print("Zadejte zeměpisnou šířku a výšku bodu, který chcete zaznamenat: \n"
-      "(pro ukončení zadávání bodů napište souřadnice (0,0)")
+print("\nZadejte zeměpisnou šířku a výšku bodu, který chcete zaznamenat: \n"
+      "(pro ukončení zadávání bodů napište souřadnice (0,0)\n")
 
 while True:
     x = float(input("z. šířka bodu: "))
     y = float(input("z. výška bodu: "))
 
+    # výpočet souřadnice bodu
     if zobrazeni_input == "Ma" or zobrazeni_input == "Po":
         if x > 0 or y > 0:
             souradnice_x = round(R * (radians(x)) * M / meritko / 3, 2)
             souradnice_y = round(R * (radians(y)) * M / meritko / 3, 2)
             print(
                 "Přepočtené souřadnice: [", souradnice_x, ";", souradnice_y, "] \n")
+            # přiřazení do seznamu (vstup pro vykreslení)
             body_X.append(souradnice_x)
             body_Y.append(souradnice_y)
 
-        elif x == 0 and y == 0:
+        elif x == 0 and y == 0: # opuštění cyklu souřadnicí (0,0)
             print("--konec zadávání bodů --")
             break
     else:
         break
 
-# ! volaní funkcí def
+# ! Volání funkcí:
 if zobrazeni_input == "Ma":
     print(Marinovo(M, R, meritko))
     print(Marinovo_body(body_X, body_Y))
